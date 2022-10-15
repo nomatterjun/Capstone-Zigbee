db48f2ccbf7b648a767fb74ea612d7c9d83b492d
이창준 <crystalashe@naver.com>
2022-10-15 23:21:12 +0900

    [Feat] 순환 참조 에러 생성

--- core/lightme/exceptions.py
+++ core/lightme/exceptions.py
@@ -20,3 +20,12 @@ class ApplicationNotFound(LoaderError):
         """Init a component not found error."""
         super().__init__(f"Component '{domain}' not found.")
         self.domain = domain
+
+class CircularDependency(LoaderError):
+    """Circular dependency is found."""
+
+    def __init__(self, from_domain: str, to_domain: str) -> None:
+        """Initialize circular dependency error."""
+        super().__init__(f"Circular dependency detected: {from_domain} -> {to_domain}.")
+        self.from_domain = from_domain
+        self.to_domain = to_domain
