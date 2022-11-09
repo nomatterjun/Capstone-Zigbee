8f231bb4ea06e3922c00579ac85625a6a0d43d44
이창준 <crystalashe@naver.com>
2022-11-09 17:01:35 +0900

    [Feat] socket 통신 타임 아웃 적용 및 예외처리

--- core/lightme/network.py
+++ core/lightme/network.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 
 import socket
 import json
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 SIZE = 1024
 
@@ -22,10 +25,17 @@ class Coordinator:
         server_address = (self._host, int(self._port))
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect(server_address)  # 서버에 접속
-            client_socket.send("Requesting state of sensor.".encode())  # 서버에 메시지 전송
-            msg = client_socket.recv(SIZE)  # 서버로부터 응답받은 메시지 반환
-            print("resp from server : {}".format(msg))  # 서버로부터 응답받은 메시지 출력
+            client_socket.settimeout(10)
+            msg: str | bytes
+            try:
+                client_socket.connect(server_address)  # 서버에 접속
+                client_socket.send("Requesting state of sensor.".encode())  # 서버에 메시지 전송
+                msg = client_socket.recv(SIZE)  # 서버로부터 응답받은 메시지 반환
+                print("resp from server : {}".format(msg))  # 서버로부터 응답받은 메시지 출력
+            except socket.timeout as error:
+                _LOGGER.error("Timeout while requesting socket connection, %s", error)
+            finally:
+                client_socket.close()
             
             dict_msg = json.loads(msg)
             self.result = dict_msg['result']
