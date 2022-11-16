e780c74d8f7f85472f8ef6c3bb9fe46506d1afaa
Eun <eunki96@naver.com>
2022-11-16 21:53:52 +0900

    창준이랑 합 다시 맞춘거
    .

--- vision/send.py
+++ vision/send.py
@@ -46,8 +46,8 @@ def calculate_Y_diff(a, b):
 
 
 # 통신 정보 설정
-IP = '10.92.81.51'
-PORT = 5050
+IP = '10.92.11.31'
+PORT = 8080
 SIZE = 1024
 ADDR = (IP, PORT)
 
@@ -58,11 +58,11 @@ ADDR = (IP, PORT)
 
 
 #비디오 설정
-stage = { "result" : str }
+stage = {
+    "CurrentMoment" : "test currnet",
+    "UpcomingMoment": "test upcoming"
+}
 temp = cv.VideoCapture(0) #카메라
-
-stage['result'] = "Initial stage"
-print(dict['result'])
 result = json.dumps(stage)
 
 
@@ -77,14 +77,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind(ADDR)  # 주소 바인딩
     server_socket.listen()  # 클라이언트의 요청을 받을 준비
     
-    client_socket, client_addr = server_socket.accept()
-    # 수신대기, 접속한 클라이언트 정보 (소켓, 주소) 반환
 
-    msg = client_socket.recv(SIZE)
-    # 클라이언트가 보낸 메시지 반환
-
-    print("[{}] message : {}".format(client_addr,msg))
-    # 클라이언트가 보낸 메시지 출력
     
     
     
@@ -138,59 +131,74 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                 
                 
                 if calculate_Y_diff_abs(right_shoulder, right_hip)<0.05 or calculate_Y_diff_abs(left_shoulder, left_hip)<0.05:
-                    stage['result'] = "lie"
+                    stage["CurrentMoment"] = "lie"
                 else:
                         
                     if (right_angle>140 and (left_angle>70 and left_angle<=140)) or (left_angle>140 and (right_angle>70 and right_angle<=140)): 
-                        stage['result'] = "sit"
+                        stage["CurrentMoment"] = "sit"
                         
                     elif (right_angle>140 and left_angle<75) or (left_angle>140 and right_angle<75): 
-                        stage['result'] = "sit"
+                        stage["CurrentMoment"] = "sit"
                         
                     elif ((right_angle>70 and right_angle<=140)and(left_angle<=75)) or ((left_angle>70 and left_angle<=140)and(right_angle<=75)): 
-                        stage['result'] = "sit"
+                        stage["CurrentMoment"] = "sit"
                                     
                     elif (right_angle>70 and right_angle<=140) and (left_angle>70 and left_angle<=140): 
                         if calculate_Y_diff(right_knee, right_hip)<=0.1:
-                            stage['result'] = "sit"
+                            stage["CurrentMoment"] = "sit"
                         else:
-                            stage['result'] = "stand"
+                            stage["CurrentMoment"] = "stand"
                             
                     elif right_angle>140 and left_angle>140:
-                        stage['result'] = "stand"
+                        stage["CurrentMoment"] = "stand"
                         
                     elif right_angle<=100 and left_angle<=100: 
                         if calculate_Y_diff(right_knee, right_hip)<=0.1 or calculate_Y_diff(left_knee, left_hip)<=0.1:
-                            stage['result'] = "sit"              
+                            stage["CurrentMoment"] = "sit"              
             except:
                 pass
                 
-            #사진 삭제
-            os.remove("temp.png")   
+ 
                 
 
-
-
-                
-            if stage["result"] == "sit":
+            if stage["CurrentMoment"] == "sit":
                 #영채 함수
                 print("sit")                
                                 
-            elif stage["result"] == "lie":
-                stage["result"] == "sleep"
+            elif stage["CurrentMoment"] == "lie":
+                stage["CurrentMoment"] == "sleep"
                 print("lie")
                 
             else:
                 print("stand")
 
 
+                
+
+
+            client_socket, client_addr = server_socket.accept()
+            # 수신대기, 접속한 클라이언트 정보 (소켓, 주소) 반환
+
+            msg = client_socket.recv(SIZE)
+            # 클라이언트가 보낸 메시지 반환
+
+            print("[{}] message : {}".format(client_addr,msg))
+            # 클라이언트가 보낸 메시지 출력
+            
+            
+            
             result = json.dumps(stage)
             client_socket.sendall(result.encode())  # 클라이언트에게 응답
-            print("전송 성공" + stage)
+            #print("전송 성공" + stage)
+            
+            
+            
+            
+            
+                        #사진 삭제
+            os.remove("temp.png")  
 
-
-
-                
+                    
         '''
         client_socket, client_addr = server_socket.accept()  # 수신대기, 접속한 클라이언트 정보 (소켓, 주소) 반환
         
